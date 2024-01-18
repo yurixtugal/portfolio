aeccff5df0efe9ddddf96295db3704a65ef64eb9
yurixtugal <yurixtugal12@gmail.com>
2024-01-18 02:33:45 -0500

    fix spaces in hero section

--- src/components/HeroPerson.tsx
+++ src/components/HeroPerson.tsx
@@ -9,12 +9,12 @@ const HeroPerson = (
   {info}: {info: HeroPersonProps}
 ) => {
   return (
-    <div className="hero min-h-screen bg-base-200" id="about">
-      <div className="hero-content flex-col lg:flex-row-reverse lg:mx-24">
+    <div className="flex items-center justify-center bg-base-200 p-16" id="about">
+      <div className="hero-content flex-col lg:flex-row-reverse lg:mx-24  xl:pt-2">
         <img src={info.image} className="max-w-sm rounded-lg shadow-2xl mb-8 lg:mb-0" />
-        <div>
+        <div className="">
           <h1 className="text-5xl font-bold">{info.title}</h1>
-          <p className="py-6">
+          <p className="py-6 ">
           {info.content}
           </p>
           <button className="btn btn-primary">{info.button}</button>
